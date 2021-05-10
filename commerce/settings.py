8e6b2b70ff78858031e424aecfa978cbd19af07d
Reeti Ranjan <reeti12109@gmail.com>
2021-05-10 20:41:52 +0530

    hosting

--- commerce/settings.py
+++ commerce/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'e-auctioncs50.herokuapp.com'
+    'e-auctioncs50.herokuapp.com',
+    '127.0.0.1'
 ]
 
 
